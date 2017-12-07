ee80c2e3b452c196652d5d17d529c585173fac0b
Inhji <johnnie@posteo.de>
2017-12-07 21:50:49 +0100

    fix user query

--- src/api/user/queries.js
+++ src/api/user/queries.js
@@ -1,6 +1,7 @@
 import User from './model'
 import { authenticationRequiredResolver } from '../resolvers'
 
-export const user = async (_, params, { user }) => {
+export const user = authenticationRequiredResolver.createResolver(async (_, params, { user }) => {
   return User.findById(user.id)
-}
+})
+
