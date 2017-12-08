c427c10c6017405f4f3eb75f28e4d73cb5ba4767
Inhji <johnnie@posteo.de>
2017-12-08 16:08:33 +0100

    fix fucking typo >:/

--- src/api/user/queries.js
+++ src/api/user/queries.js
@@ -2,6 +2,5 @@ import User from './model'
 import { authenticationRequiredResolver } from '../resolvers'
 
 export const user = authenticationRequiredResolver.createResolver(async (_, params, { user }) => {
-  return User.findById(user.id)
+  return User.findById(user._id)
 })
-
