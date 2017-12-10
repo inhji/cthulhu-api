d110f82280749dc93858b2dd735fbb78cf10c0c3
Inhji <johnnie@posteo.de>
2017-12-10 15:02:24 +0100

    add naíve error handlers

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import jwt from 'express-jwt'
 import bodyParser from 'body-parser'
+import errorHandler from 'errorhandler'
 import cors from 'cors'
 import router from './router'
 import logger from './logger'
@@ -12,6 +13,7 @@ export const start = async () => {
 
     const app = express()
     const port = process.env.PORT
+    const dev = process.env.NODE_ENV === 'development'
 
     app.use(bodyParser.json())
     app.use(cors({ origin: true }))
@@ -21,11 +23,20 @@ export const start = async () => {
         credentialsRequired: false
       })
     )
-
     app.use('/', router)
+    if (dev) {
+      app.use(errorHandler({ log: true }))
+    } else {
+      app.use((err, req, res, next) => {
+        logger.error(err.stack)
+        res.sendStatus(500).send('Something broke horribly!')
+      })
+    }
+
     app.listen(port)
+
     logger.info(`Server started on port ${port}`)
   } catch (e) {
-    logger.error(`Error while starting server: ${e.message}` )
+    logger.error(`Error while starting server: ${e.message}`)
   }
 }
