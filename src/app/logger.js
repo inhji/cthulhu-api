3805df655c1241da30937fe825fcf6b839a21fe2
Inhji <johnnie@posteo.de>
2017-12-15 16:43:56 +0100

    winston sucks...

--- src/app/logger.js
+++ src/app/logger.js
@@ -1,24 +1,21 @@
 import { createLogger, format, transports } from 'winston'
 const { combine, timestamp, prettyPrint, label, printf, json } = format
 
-const consoleFormat = combine(
-  timestamp(),
-  printf(info => {
-    return `${info.timestamp} ${info.level}: ${info.message}`
-  })
-)
-
 const logger = createLogger({
   level: 'info',
+  format: combine(
+    timestamp(),
+    printf(info => {
+      return `${info.timestamp} ${info.level}: ${info.message}`
+    })
+  ),
   transports: [
-    new transports.Console({ format: consoleFormat }),
+    new transports.Console(),
     new transports.File({
-      format: json(),
       filename: 'log/error.log',
       level: 'error'
     }),
     new transports.File({
-      format: json(),
       filename: 'log/combined.log'
     })
   ]
