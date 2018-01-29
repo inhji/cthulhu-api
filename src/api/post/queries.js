73fec87c9c1fcafc8cb33fa6c98f13dab45c1f6c
Inhji <johnnie@posteo.de>
2018-01-29 22:57:48 +0100

    populate post author

--- src/api/post/queries.js
+++ src/api/post/queries.js
@@ -2,8 +2,12 @@ import Post from './model'
 
 export const posts = async () => {
   return Post.find()
+    .populate('author')
+    .exec()
 }
 
 export const post = (_, { id }) => {
   return Post.findOne({ _id: id })
+    .populate('author')
+    .exec()
 }
