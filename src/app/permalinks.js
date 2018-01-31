bf823aedda8c7a9e38b2d687728599b0e007d238
Inhji <johnnie@posteo.de>
2018-01-31 22:56:12 +0100

    fix env var - closes #37

--- src/app/permalinks.js
+++ src/app/permalinks.js
@@ -1,5 +1,5 @@
 import url from 'url'
 
 export function generatePermalink ({ hashid, type }) {
-  return url.resolve(`${process.env.BASE}`, type.toLowerCase(), hashid)
+  return url.resolve(`${process.env.BASE_URL}`, type.toLowerCase(), hashid)
 }
