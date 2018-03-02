2c6120abbf9f6b03ca6409946c2a93e596756482
Inhji <johnnie@posteo.de>
2018-03-02 22:58:18 +0100

    git jk on emojis - move to homepage

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import path from 'path'
-import serveStatic from 'serve-static'
 import bodyParser from 'body-parser'
 import errorHandler from 'errorhandler'
 import morgan from 'morgan'
@@ -26,7 +24,6 @@ export const start = async () => {
     // Pipe morgan output into bunyan :)
     app.use(morgan('tiny', { stream: logger.stream }))
     app.use(bodyParser.json())
-    app.use('/assets', serveStatic(path.join(__dirname, '../..', 'assets')))
     app.use('/', router)
     app.use(
       dev
