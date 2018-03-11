2fef3526b4f67587846eb68719bc9cf14acfd90f
Inhji <johnnie@posteo.de>
2018-03-11 16:24:14 +0100

    prettier

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 require('babel-polyfill')
 require('babel-register')({
-	presets: ['env'],
-	plugins: ['transform-object-rest-spread']
+  presets: ['env'],
+  plugins: ['transform-object-rest-spread']
 })
 
 const { start } = require('./src/app')
