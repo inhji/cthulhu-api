270fd98ab8acded7043e3e92cd5709363117af10
Inhji <johnnie@posteo.de>
2018-03-28 20:00:48 +0200

    use dotenv-safe

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
-require('babel-polyfill')
-require('babel-register')({
-  presets: ['env'],
-  plugins: ['transform-object-rest-spread']
-})
+try {
+  require('dotenv-safe').config()
+  require('babel-polyfill')
+  require('babel-register')({
+    presets: ['env'],
+    plugins: ['transform-object-rest-spread']
+  })
 
-const { start } = require('./src')
+  const { start } = require('./src')
 
-start()
+  start()
+} catch (e) {
+  console.error(e.message)
+}
