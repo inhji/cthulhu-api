4919ef931e38052a34ba570022bbb7c0026f0120
Jonathan Jenne <johnnie@posteo.de>
2018-03-31 12:53:15 +0200

    don't log requests while testing

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,18 @@
 import { createLogger, stdSerializers } from 'bunyan'
 
-const logger = createLogger({ name: 'app', serializers: stdSerializers })
+const logger = createLogger({
+  name: 'app',
+  serializers: stdSerializers,
+  streams: [
+    // Only log errors in test env
+    process.env.NODE_ENV !== 'test'
+      ? {
+        stream: process.stdout,
+        level: 'info'
+      }
+      : { stream: process.stdout, level: 'error' }
+  ]
+})
 const reqLogger = logger.child({ component: 'req' })
 
 logger.stream = {
